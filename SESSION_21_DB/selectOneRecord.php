9c179973ac5c5c0e5f760a866857e95845fa3a0e
lauramarianella <lauramarianella@gmail.com>
2021-03-18 00:43:11 -0400

    select 1 record

--- SESSION_21_DB/selectOneRecord.php
+++ SESSION_21_DB/selectOneRecord.php
@@ -1,21 +1,4 @@
-<?php
-$mySqlhost  = "localhost:3306"; //127.0.0.1
-$username   = "root";
-$password   = "";
-$myDB       = "mystoredb2";
-$charset = "utf8mb4";
-$dsn = "mysql:host=$mySqlhost;dbname=$myDB;charset=$charset"; //data source name
-try {
-    $connect = new PDO($dsn, $username, $password);
-    // set the PDO error mode to exception
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch (PDOException $e) {
-    echo "<h1 class=\"text-danger\">Connection failed: </h1>" . "<p>" . $e->getMessage() . "</p>";
-    //throw new PDOException($e->getMessage());
-}
-
-?>
+<?php include "_dbConnection.php" ?>
 
 <!DOCTYPE html>
 <html lang="en">
